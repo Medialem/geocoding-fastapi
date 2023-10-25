864cc8c711760f96d7371023b4c2fb30cdc96e53
Hamza <ham_cher@outlook.fr>
2023-10-25 16:27:22 +0100

    get all results

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import datetime
 
 def google_map_geocoding(query, token):
     try:
@@ -12,13 +13,42 @@ def google_map_geocoding(query, token):
             pb=token
         )
         response = requests.get(url=url, params=params)
+        request_id = datetime.now().strftime("%Y%m%d%H%M%S")
         if response.status_code == 200:
             content = response.text.replace(')]}\'\n', '')
             data = json.loads(content)
+
+            item = data[0][1]
+            result = []
+            index = 0
+            for i in range(0, len(item)):
+                adr = dict()
+                try:
+                    element = item[i][14]
+                    if element[30] == 'Africa/Algiers':
+                        adr = {
+                            'query': query,
+                            'request_id': request_id,
+                            'index': index,
+                            "coordinate": [element[9][2], element[9][3]],
+                            'data_0': element[0],
+                            'data_1': element[1],
+                            'data_2': element[2],
+                            'data_3': element[10],
+                            'data_4': element[11],
+                            'data_5': element[13],
+                            'data_6': element[14],
+                            'data_7': element[18],
+                        }
+                        index += 1
+                except Exception as e:
+                    pass
+                if adr:
+                    result.append(adr)
+
             return dict(
                 type="success",
-                coordinate=[data[0][1][0][14][9][2], data[0][1][0][14][9][3]],
-                address=data[0][1][0][14][11]
+                data=result
             )
         else:
             return dict(
@@ -30,12 +60,12 @@ def google_map_geocoding(query, token):
         print("Exception: ", e)
         return dict(
             type='error',
-            satus=500,
+            status=500,
             message=str(e)
         )
     return dict(
         type='error',
-        status="400",
+        status=400,
         message="query not valide",
     )
 
